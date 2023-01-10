bd955ab2b06c8dc5c04189204823e547592b0cd4
taiprogrammer <taiza.marques@sptech.school>
2023-01-10 18:31:39 -0300

    summary component

--- src/pages/Transactions/Index.tsx
+++ src/pages/Transactions/Index.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../../components/Header/Index";
+import { Summary } from "../../components/Summary/Index";
 
 export function Transaction() {
   return (
     <div>
       <Header />
+      <Summary />
     </div>
   );
 }
