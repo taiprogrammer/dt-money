ebc90c98ea327ff3881ec7f45916022d2c9c6fba
taiprogrammer <taiza.marques@sptech.school>
2023-01-10 19:04:15 -0300

    transactions table component

--- src/pages/Transactions/Index.tsx
+++ src/pages/Transactions/Index.tsx
@@ -1,11 +1,38 @@
 import { Header } from "../../components/Header/Index";
 import { Summary } from "../../components/Summary/Index";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transaction() {
   return (
     <div>
       <Header />
       <Summary />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">-R$ 50,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
